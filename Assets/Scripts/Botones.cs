5bcb28a03a25cd6611a832442663df281e52fdd1
Istanolion <akuma_diego@hotmail.com>
2018-08-13 20:54:36 -0500

    finished

--- Assets/Scripts/Botones.cs
+++ Assets/Scripts/Botones.cs
@@ -4,98 +4,238 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class Botones : MonoBehaviour {
-    public Text texto;
-    string linea;
+    public Text texto,texto2;
+    string linea, Base;
+    public Logica logic;
+    public GameObject Bina, Octa, Deca, Hexa;
     private void Awake()
     {
         linea = "";
+        Base = "Decimal";
+        Bina.SetActive(false);
+        Octa.SetActive(false);
+        Hexa.SetActive(false);
     }
     public void Boton0()
     {
-        if (linea != "")
+        if (linea != "" ||linea.Length<15 )
         {
-            linea = string.Concat(linea, "0");
-            texto.text = linea;
+                linea = string.Concat(linea, "0");
+                texto.text = linea;
+                AgregarNum(0);
         }
     }
     public void Boton1()
     {
-        linea = string.Concat(linea, "1");
-        texto.text = linea;
+        if (linea.Length < 15)
+        {
+            linea = string.Concat(linea, "1");
+            texto.text = linea;
+            AgregarNum(1);
+        }
     }
     public void Boton2()
     {
-        linea = string.Concat(linea, "2");
-        texto.text = linea;
+        if (linea.Length < 15)
+        {
+            linea = string.Concat(linea, "2");
+            texto.text = linea;
+            AgregarNum(2);
+        }
     }
     public void Boton3()
     {
-        linea = string.Concat(linea, "3");
-        texto.text = linea;
+        if (linea.Length < 15)
+        {
+            linea = string.Concat(linea, "3");
+            texto.text = linea;
+            AgregarNum(3);
+        }
     }
     public void Boton4()
     {
-        linea = string.Concat(linea, "4");
-        texto.text = linea;
+        if (linea.Length < 15)
+        {
+            linea = string.Concat(linea, "4");
+            texto.text = linea;
+            AgregarNum(4);
+        }
     }
     public void Boton5()
     {
-        linea = string.Concat(linea, "5");
-        texto.text = linea;
+        if (linea.Length < 15)
+        {
+            linea = string.Concat(linea, "5");
+            texto.text = linea;
+            AgregarNum(5);
+        }
     }
     public void Boton6()
     {
-        linea = string.Concat(linea, "6");
-        texto.text = linea;
+        if (linea.Length < 15)
+        {
+            linea = string.Concat(linea, "6");
+            texto.text = linea;
+            AgregarNum(6);
+        }
     }
     public void Boton7()
     {
-        linea = string.Concat(linea, "7");
-        texto.text = linea;
+        if (linea.Length < 15)
+        {
+            linea = string.Concat(linea, "7");
+            texto.text = linea;
+            AgregarNum(7);
+        }
     }
     public void Boton8()
     {
-        linea = string.Concat(linea, "8");
-        texto.text = linea;
+        if (linea.Length < 15)
+        {
+            linea = string.Concat(linea, "8");
+            texto.text = linea;
+            AgregarNum(8);
+        }
     }
     public void Boton9()
     {
-        linea = string.Concat(linea, "9");
-        texto.text = linea;
+        if (linea.Length < 15)
+        {
+            linea = string.Concat(linea, "9");
+            texto.text = linea;
+            AgregarNum(9);
+        }
     }
     public void Boton10()
     {
-        linea = string.Concat(linea, "A");
-        texto.text = linea;
+        if (linea.Length < 15)
+        {
+            linea = string.Concat(linea, "A");
+            texto.text = linea;
+            AgregarNum(10);
+        }
     }
     public void Boton11()
     {
-        linea = string.Concat(linea, "B");
-        texto.text = linea;
+        if (linea.Length < 15)
+        {
+            linea = string.Concat(linea, "B");
+            texto.text = linea;
+            AgregarNum(11);
+        }
     }
     public void Boton12()
     {
-        linea = string.Concat(linea, "C");
-        texto.text = linea;
+        if (linea.Length < 15)
+        {
+            linea = string.Concat(linea, "C");
+            texto.text = linea;
+            AgregarNum(12);
+        }
     }
     public void Boton13()
     {
-        linea = string.Concat(linea, "D");
-        texto.text = linea;
+        if (linea.Length < 15)
+        {
+            linea = string.Concat(linea, "D");
+            texto.text = linea;
+            AgregarNum(13);
+        }
     }
     public void Boton14()
     {
-        linea = string.Concat(linea, "E");
-        texto.text = linea;
+        if (linea.Length < 15)
+        {
+            linea = string.Concat(linea, "E");
+            texto.text = linea;
+            AgregarNum(14);
+        }
     }
     public void Boton15()
     {
-        linea = string.Concat(linea, "F");
-        texto.text = linea;
+        if (linea.Length < 15)
+        {
+            linea = string.Concat(linea, "F");
+            texto.text = linea;
+            AgregarNum(15);
+        }
     }
     public void BotonPunto()
     {
-        linea = string.Concat(linea, ".");
+        if (linea.Length < 15 && !linea.Contains("."))
+        {
+            linea = string.Concat(linea, ".");
+            texto.text = linea;
+        }
+    }
+    public void Decimal()
+    {
+        if (linea.Length == 0)
+        {
+            GameObject.Find(Base).SetActive(false);
+            Deca.SetActive(true);
+            Base = "Decimal";
+        }
+        else
+        {
+            texto2.text = logic.XtoDec(Base);
+            Clear();
+        }
+    }
+    public void Bin()
+    {
+        if (linea.Length == 0)
+        {
+            GameObject.Find(Base).SetActive(false);
+            Bina.SetActive(true);
+            Base = "Binario";
+        }
+        else
+        {
+            texto2.text = logic.XtoBin(Base);
+            Clear();
+        }
+    }
+    public void Oct()
+    {
+        if (linea.Length == 0)
+        {
+            GameObject.Find(Base).SetActive(false);
+            Octa.SetActive(true);
+            Base = "Octal";
+        }
+        else
+        {
+            texto2.text = logic.XtoOct(Base);
+            Clear();
+        }
+    }
+    public void Hex()
+    {
+        if (linea.Length == 0)
+        {
+            GameObject.Find(Base).SetActive(false);
+            Hexa.SetActive(true);
+            Base = "Hexa";
+        }
+        else
+        {
+            texto2.text = logic.XtoHex(Base);
+            Clear();
+        }
+    }
+   public  void Clear()
+    {
+        linea = "";
         texto.text = linea;
+    }
+    void AgregarNum(int num)
+    {
+        if (linea.Contains("."))
+        {
+            logic.AgregarDecimal(num);
+        }
+        else
+            logic.AgregarEntero(num);
     }
 }
---
+++ Assets/Scripts/Logica.cs
@@ -0,0 +1,223 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Logica : MonoBehaviour {
+    Queue<int> enteros= new Queue<int>();
+    Stack<int> decimales = new Stack<int>();
+    int Numero = 0;
+    float deci=0,transformacion;
+    string texto;
+    public void AgregarEntero(int num)
+    {
+        enteros.Enqueue(num);
+    }
+    public void AgregarDecimal(int num)
+    {
+        decimales.Push(num);
+    }
+    public string XtoDec(string basnum)
+    {
+        int basenum=0;
+        switch (basnum)
+        {
+            case ("Decimal"):
+                basenum = 10;
+                break;
+            case ("Binario"):
+                basenum = 2;
+                break;
+            case ("Octal"):
+                basenum = 8;
+                break;
+            case ("Hexa"):
+                basenum = 16;
+                break;
+        }
+        for(int i = enteros.Count-1; i >= 0; i--)
+        {
+            Numero += (int)Mathf.Pow(basenum, i) * enteros.Dequeue();
+        }
+        for(int i = decimales.Count; i >= 1; i--)
+        {
+            deci += Mathf.Pow(basenum, -i) * decimales.Pop();
+        }
+        texto = "";
+        if (deci != 0)
+        {
+            texto = deci.ToString();
+            texto = texto.Substring(1);
+        }
+        texto= string.Concat(Numero.ToString(),texto);
+        transformacion = Numero + deci;
+        Numero = 0;
+        deci = 0;
+        return texto;
+    }
+    public string XtoHex(string basnum)
+    {
+        XtoDec(basnum);
+        int entero = Mathf.FloorToInt(transformacion);
+        transformacion -= entero;
+        Stack<string> chars = new Stack<string>();
+        do
+        {
+            if (entero % 16 < 10)
+            {
+                int num = entero % 16;
+                chars.Push(num.ToString());
+            }
+            else
+            {
+                int num = entero % 16;
+                switch (num)
+                {
+                    case (10):
+                        chars.Push("A");
+                        break;
+                    case (11):
+                        chars.Push("B");
+                        break;
+                    case (12):
+                        chars.Push("C");
+                        break;
+                    case (13):
+                        chars.Push("D");
+                        break;
+                    case (14):
+                        chars.Push("E");
+                        break;
+                    case (15):
+                        chars.Push("F");
+                        break;
+                }
+            }
+            entero = Mathf.FloorToInt(entero / 16);
+        } while (entero > 15);
+        chars.Push(entero.ToString());
+        Queue<string> let = new Queue<string>();
+        int i = 0;
+        do
+        {
+            transformacion *= 16;
+            int num = Mathf.FloorToInt(transformacion);
+            transformacion -= num;
+            if(num<10)
+                let.Enqueue(num.ToString());
+            else
+            {
+                switch (num)
+                {
+                    case (10):
+                        let.Enqueue("A");
+                        break;
+                    case (11):
+                        let.Enqueue("B");
+                        break;
+                    case (12):
+                        let.Enqueue("C");
+                        break;
+                    case (13):
+                        let.Enqueue("D");
+                        break;
+                    case (14):
+                        let.Enqueue("E");
+                        break;
+                    case (15):
+                        let.Enqueue("F");
+                        break;
+                }
+            }
+            i++;
+        } while (i<5);
+        texto = "";
+        for (i = chars.Count; i >0 ; i--)
+        {
+            texto = string.Concat(texto, chars.Pop());
+        }
+        texto = string.Concat(texto, ".");
+        for (i=let.Count;i>0;i--)
+        {
+            texto = string.Concat(texto, let.Dequeue());
+        }
+        return texto;
+    }
+    public string XtoOct(string basnum)
+    {
+        XtoDec(basnum);
+        int entero = Mathf.FloorToInt(transformacion);
+        transformacion -= entero;
+        Stack<string> chars = new Stack<string>();
+        do
+        {
+            if (entero % 8 < 8)
+            {
+                int num = entero % 8;
+                chars.Push(num.ToString());
+            }
+            entero = Mathf.FloorToInt(entero / 8);
+        } while (entero > 7);
+        chars.Push(entero.ToString());
+        Queue<string> let = new Queue<string>();
+        int i = 0;
+        do
+        {
+            transformacion *= 8;
+            int num = Mathf.FloorToInt(transformacion);
+            transformacion -= num;
+            if (num < 8)
+                let.Enqueue(num.ToString());
+            i++;
+        } while (i < 5);
+        texto = "";
+        for (i = chars.Count; i > 0; i--)
+        {
+            texto = string.Concat(texto, chars.Pop());
+        }
+        texto = string.Concat(texto, ".");
+        for (i = let.Count; i > 0; i--)
+        {
+            texto = string.Concat(texto, let.Dequeue());
+        }
+        return texto;
+    }
+    public string XtoBin(string basnum)
+    {
+        XtoDec(basnum);
+        int entero = Mathf.FloorToInt(transformacion);
+        transformacion -= entero;
+        Stack<string> chars = new Stack<string>();
+        do
+        {
+            if (entero % 2 < 2)
+            {
+                int num = entero % 2;
+                chars.Push(num.ToString());
+            }
+            entero = Mathf.FloorToInt(entero / 2);
+        } while (entero > 1);
+        chars.Push(entero.ToString());
+        Queue<string> let = new Queue<string>();
+        int i = 0;
+        do
+        {
+            transformacion *= 2;
+            int num = Mathf.FloorToInt(transformacion);
+            transformacion -= num;
+            if (num < 2)
+                let.Enqueue(num.ToString());
+            i++;
+        } while (i < 5);
+        texto = "";
+        for (i = chars.Count; i > 0; i--)
+        {
+            texto = string.Concat(texto, chars.Pop());
+        }
+        texto = string.Concat(texto, ".");
+        for (i = let.Count; i > 0; i--)
+        {
+            texto = string.Concat(texto, let.Dequeue());
+        }
+        return texto;
+    }
+}
